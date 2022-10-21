a66adc151c739c42a062b69152df04d04abe5471
unknown <tyler.r.roll@gmail.com>
2022-10-21 11:32:34 -0600

    fix wrong pathing

--- pages/constants/abiUtil.js
+++ pages/constants/abiUtil.js
@@ -1,4 +1,4 @@
-import ERC20ABI from '../assets/erc20-abi.json'
+import ERC20ABI from '../assets/tokenAbi.json'
 
 export function getERC20Contract(tokenAddress, web3){
   return web3 ? new web3.eth.Contract(ERC20ABI, tokenAddress, {
